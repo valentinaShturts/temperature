fd6f0f85eedd8f5a4da47e8490b23ff3bf563f61
valentinaShturts <valentinashturts@gmail.com>
2025-02-10 19:22:43 +0200

    Add project files.

---
+++ operation/operation.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
+#pragma comment(lib, "urlmon.lib")
+
+using namespace std;
+
+
+string ExtractTemperature(const char* html) 
+{
+    const char* marker = "<p class=\"_6fYCPKSx\">";
+    char* start = strstr((char*)html, marker);
+    if (start) 
+    {
+        start += strlen(marker);
+        char* end = strstr(start, "</p>");
+        if (end) 
+        {
+            return string(start, end - start-3);
+        }
+    }
+    return "Not found";
+}
+
+int main() 
+{
+    const char* srcURL = "https://sinoptik.ua/pohoda/odesa"; 
+    const char* destFile = "weather.txt";
+
+    if (S_OK == URLDownloadToFileA(NULL, srcURL, destFile, 0, NULL)) 
+    {
+        cout << "Saved to " << destFile << "\n";
+    }
+
+    ifstream file(destFile); 
+    if (!file) 
+    {
+        cout << "Failed to open file!" << endl;
+        return 1;
+    }
+
+    string html((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+    file.close();
+
+    cout << "Temperature Odesa: " << ExtractTemperature(html.c_str()) << endl;
+
+    return 0;
+}
